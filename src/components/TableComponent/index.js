908a84702bde4c9e7f4d9ce1cd7f4b102a21e690
prady <pradyumna.raparti@gmail.com>
2022-05-08 22:54:42 -0700

    search added

--- src/components/TableComponent/index.js
+++ src/components/TableComponent/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
@@ -6,6 +6,8 @@ import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@material-ui/core/Paper'
+import SearchBar from 'material-ui-search-bar'
+import { styled } from '@mui/material/styles'
 import { styles } from './styles.scss'
 
 const TableComponent = () => {
@@ -18,17 +20,43 @@ const TableComponent = () => {
       protein
     }
   }
-
-  const rows = [
+  const ogRows = [
     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
     createData('Eclair', 262, 16.0, 24, 6.0),
     createData('Cupcake', 305, 3.7, 67, 4.3),
     createData('Gingerbread', 356, 16.0, 49, 3.9)
   ]
+  const [rows, setRows] = useState(ogRows)
+  const [searched, setSearched] = useState('')
+  const requestSearch = (searchedVal) => {
+    const filteredRows = ogRows.filter((row) => {
+      return row.name.toLowerCase().includes(searchedVal.toLowerCase())
+    })
+    setRows(filteredRows)
+  }
+  const cancelSearch = () => {
+    setSearched('')
+    requestSearch(searched)
+  }
+
+  const StyledTableRow = styled(TableRow)(() => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: '#737070'
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0
+    }
+  }))
 
   return (
     <div className={styles}>
+      <SearchBar
+        value={searched}
+        onChange={searchVal => requestSearch(searchVal)}
+        onCancelSearch={() => cancelSearch()}
+      />
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -42,7 +70,7 @@ const TableComponent = () => {
           </TableHead>
           <TableBody>
             {rows.map(row => (
-              <TableRow
+              <StyledTableRow
                 key={row.name}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
@@ -53,7 +81,7 @@ const TableComponent = () => {
                 <TableCell align="right">{row.fat}</TableCell>
                 <TableCell align="right">{row.carbs}</TableCell>
                 <TableCell align="right">{row.protein}</TableCell>
-              </TableRow>
+              </StyledTableRow>
             ))}
           </TableBody>
         </Table>
