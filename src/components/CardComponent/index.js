ca85f279c0359489a38c6c894e127547db4681bd
prady <pradyumna.raparti@gmail.com>
2022-05-11 03:11:13 -0700

    dashboard cards

--- src/components/CardComponent/index.js
+++ src/components/CardComponent/index.js
@@ -1,11 +1,15 @@
 import React                   from 'react'
+import PropTypes from 'prop-types'
 import { Card, CardContent, Typography } from '@material-ui/core'
 import { styles }              from './styles.scss'
 
-const CardComponent = () => {
+const CardComponent = (props) => {
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
   return (
     <div className={styles}>
-      <Card sx={{ backgroundColor: '#1D2024' }}>
+      <Card sx={{ backgroundColor: '#1D2024'}}>
         <CardContent
           sx={{ paddingLeft: '0px', paddingRight: '0px', paddingTop: '0px' }}
         >
@@ -13,15 +17,24 @@ const CardComponent = () => {
             component="div"
             variant="body1"
           >
-            Total Projects
+            {props.title}
           </Typography>
+          <Typography sx={{ mb: 1.5, size: '10px', color: '#505050'}} variant='caption'>
+           {props.caption}
+        </Typography>
           <Typography sx={{ padding: '8px', color: 'white' }} variant="h3">
-            10
+            {numberWithCommas(props.data)}
           </Typography>
         </CardContent>
       </Card>
     </div>
   )
+}
+
+CardComponent.propTypes = {
+  title: PropTypes.string,
+  data: PropTypes.number,
+  caption: PropTypes.string
 }
 
 export default CardComponent
--- src/containers/HomeView/index.js
+++ src/containers/HomeView/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Box                  from '@mui/material/Box'
 import Grid                 from '@mui/material/Grid'
 import Paper                from '@mui/material/Paper'
 import { styled }           from '@mui/material/styles'
 import { Typography }       from '@material-ui/core'
+import { Button }           from '@mui/material'
+import { query, collection, onSnapshot, where } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useAuth } from "../../contexts/AuthContext";
 import CardComponent        from '../../components/CardComponent'
 import TableComponent       from '../../components/TableComponent'
-import { Button } from '@mui/material'
 
 const Item = styled(Paper)(({ theme }) => ({
   marginBottom: theme.spacing(1)
@@ -18,30 +21,65 @@ const HomeViewTypography = styled(Typography)(({ theme }) => ({
 }))
 
 const HomeView = () => {
-    return (
-      <div className="container">
-        <HomeViewTypography variant="h6">
-          NGO Dashboard
-        </HomeViewTypography>
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid container columnSpacing={14}>
-            <Grid item xs={4}>
-              <Item><CardComponent /></Item>
-            </Grid>
-            <Grid item xs={4}>
-              <Item><CardComponent /></Item>
-            </Grid>
-            <Grid item xs={4}>
-              <Item><CardComponent /></Item>
-            </Grid>
+  const [collectionData, setCollectionData] = useState([]);
+  const [totalBudget, setTotalBudget] = useState(0);
+  const [collectionUnsub, setCollectionUnsub] = useState({ f: null });
+  const currentUser = useAuth().currentUser;
+  
+  function getCollectionData() {
+    if (collectionUnsub.f) collectionUnsub.f();
+    const unsub = onSnapshot(
+      query(collection(db, `projects`)),
+      (querySnapshot) => {
+        const projects = [];
+        let budget = 0;
+        querySnapshot.forEach((doc) => {
+          console.log(doc.data());
+          projects.push(doc.data());
+          budget += parseInt(doc.data().budget);
+        });
+        setCollectionData(projects);
+        setTotalBudget(budget);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    setCollectionUnsub({ f: unsub });
+  }
+
+  useEffect(() => {
+    getCollectionData();
+    return function cleanup() {
+      if (collectionUnsub.f) collectionUnsub.f();
+    };
+  }, [currentUser]);
+  
+  return (
+    <div className="container">
+      <HomeViewTypography variant="h6">
+        NGO Dashboard
+      </HomeViewTypography>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container columnSpacing={14}>
+          <Grid item xs={4}>
+            <Item><CardComponent title={'total projects'} data={collectionData.length} caption={" "}/></Item>
           </Grid>
-        </Box>
-        <HomeViewTypography variant="subtitle1">
-          Projects
-        </HomeViewTypography>
-        <TableComponent />
-      </div>
-    )
+          <Grid item xs={4}>
+            <Item><CardComponent title={'total spend'} data={10000} caption={"in dollars"}/></Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item><CardComponent title={'total budget'} data={totalBudget} caption={"in dollars"}/></Item>
+          </Grid>
+        </Grid>
+      </Box>
+      <HomeViewTypography variant="subtitle1">
+        Projects
+      </HomeViewTypography>
+      <TableComponent />
+    </div>
+  );
 }
 
 export default HomeView;
